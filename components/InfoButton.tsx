bf8b050f0cec9bac81388f1d57e1d4aec8415e94
thientantran <thientantm@gmail.com>
2023-11-14 17:35:20 +0700

    Layout  - Modal

--- components/InfoButton.tsx
+++ components/InfoButton.tsx
@@ -10,8 +10,8 @@ export default function InfoButton({data}: InfoButtonProps) {
   const { openModal } = useInfoModal();
 
   const handleOpenModal = useCallback(() => {
-    openModal(data?.id);
-  }, [openModal, data?.id]);
+    openModal(data);
+  }, [openModal, data]);
   return (
     <button onClick={handleOpenModal} className="bg-white text-white bg-opacity-30 rounded-xl py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition">
             <InfoIcon className="mr-1 w-4 md:w-7"/>
--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,6 +1,8 @@
 'use client'
-import { ShieldCloseIcon } from 'lucide-react'
+import useInfoModal from '@/hooks/useInfoModal'
+import { XIcon } from 'lucide-react'
 import { useCallback, useEffect, useState } from 'react'
+import PlayButton from './PlayButton'
 
 
 interface InfoModalProps{
@@ -9,6 +11,7 @@ interface InfoModalProps{
 }
 export default function InfoModal({visible, onClose}: InfoModalProps) {
   const [isVisible, setIsVisible] = useState(Boolean(visible))
+  const {movie: data} = useInfoModal()
   useEffect(() => {
     setIsVisible(!!visible);
   }, [visible]);
@@ -26,10 +29,42 @@ export default function InfoModal({visible, onClose}: InfoModalProps) {
 
   return (
     <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
-      <div onClick={handleClose} className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center">
-          <ShieldCloseIcon className="text-white w-6" />
-          <p className='text-white'>Click here</p>
+      <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
+        <div className={`${isVisible ? 'scale-100' : 'scale-0'} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
+
+          <div className="relative h-96">
+            <video poster={data?.thumbnailUrl} autoPlay muted loop src={data?.videoUrl} className="w-full brightness-[60%] object-cover h-full" />
+            <div onClick={handleClose} className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center">
+              <XIcon className="text-white w-6" />
+            </div>
+            <div className="absolute bottom-[10%] left-10">
+              <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
+                {data?.title}
+              </p>
+              <div className="flex flex-row gap-4 items-center">
+                <PlayButton movieId={data?.id} />
+                {/* <FavoriteButton movieId={data?.id} favoriteMovies={favoriteMovieIds} /> */}
+              </div>
+            </div>
+          </div>
+          <div className="px-12 py-8">
+            <div className="flex flex-row items-center gap-2 mb-8">
+              <p className="text-green-400 font-semibold text-lg">
+                New
+              </p>
+              <p className="text-white text-lg">
+                {data?.duration}
+              </p>
+              <p className="text-white text-lg">
+                {data?.genre}
+              </p>
+            </div>
+            <p className="text-white text-lg">
+              {data?.description}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
